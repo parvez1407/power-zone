4d59b353ca1e6c416bb07f648d25555496f73dfc
parvez1407 <info.nahidwd@gmail.com>
2022-09-28 23:47:04 +0600

    added logo and name of the site and divided home two part exercises and list orcart

---
+++ src/components/Home/Home.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import './Home.css';
+import logo from '../../logo.png'
+
+const Home = () => {
+    return (
+        <div className='home-container'>
+            <div className='exercises-container'>
+                <div className='flex'>
+                    <img className='w-14 h-10' src={logo} alt="" />
+                    <h1 className='text-4xl text-violet-700 font-black'>POWER-ZONE</h1>
+                </div>
+                <div className='all-exercises'>
+                    <h2 className='text-2xl mt-7 font-bold text-slate-700'>Select Your Activity</h2>
+                </div>
+            </div>
+            <div className='info-cart'>
+                <h1>cart section</h1>
+            </div>
+        </div>
+    );
+};
+
+export default Home;
