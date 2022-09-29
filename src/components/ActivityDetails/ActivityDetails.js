afcfda118abd5a007d5d2f5740b212e7f277009f
parvez1407 <info.nahidwd@gmail.com>
2022-09-29 18:45:08 +0600

    some correction of activity details

--- src/components/ActivityDetails/ActivityDetails.js
+++ src/components/ActivityDetails/ActivityDetails.js
@@ -5,27 +5,34 @@ import Swal from 'sweetalert2'
 
 
 const ActivityDetails = ({ count }) => {
-    const [time, setTime] = useState([]);
-    console.log(time);
+
+    const [breakTime, setBreakTime] = useState([]);
+    // console.log(time);
     let totalTime = 0;
     for (const exercise of count) {
         totalTime = totalTime + exercise.time;
     }
 
     useEffect(() => {
-        const restTi = localStorage.getItem('restTime');
-        setTheTime(restTi);
-    }, [time])
+        const restTime = localStorage.getItem('restTime');
+        setTheTime(restTime);
+    }, [breakTime])
 
-    const setTheTime = time => {
-        localStorage.setItem('restTime', time);
-        setTime(time);
+    const setTheTime = breakTime => {
+        if (breakTime) {
+
+            localStorage.setItem('restTime', breakTime);
+            setBreakTime(breakTime);
+        }
+        else {
+            setBreakTime(0);
+        }
     }
 
     const handleExercise = () => {
         Swal.fire(
             'WoW, Great!',
-            'Today Your All Activity Completed!',
+            'Today Your All Activity has been Completed!',
             'success'
         )
     }
@@ -71,7 +78,7 @@ const ActivityDetails = ({ count }) => {
                 </div>
                 <div className='flex justify-between bg-slate-200 p-3 rounded-md mt-5'>
                     <p>Resting Time</p>
-                    <p className='text-slate-400'>{time} seconds</p>
+                    <p className='text-slate-400'>{breakTime} seconds</p>
                 </div>
             </div>
             <div className='text-center mt-10'>
